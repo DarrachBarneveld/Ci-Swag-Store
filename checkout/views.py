91247c49aac26334a928f08af06aebc1de22da44
DarrachBarneveld <130994206+DarrachBarneveld@users.noreply.github.com>
2023-12-20 14:53:29 +0000

    Added stripe card to form

--- checkout/views.py
+++ checkout/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
+from django.conf import settings
 from .forms import OrderForm
+
+
+from cart.contexts import cart_contents
+
+import stripe
+
 
 # Create your views here.
 def checkout(request):
@@ -7,10 +14,31 @@ def checkout(request):
     stripe_public_key = settings.STRIPE_PUBLIC_KEY
     stripe_secret_key = settings.STRIPE_SECRET_KEY
 
+    current_cart = cart_contents(request)
+
+    total = current_cart['total']
+    stripe_total = round(total * 100)
+
+
+    print(stripe_total)
+
+
+
+
     order_form = OrderForm()
+    stripe.api_key = stripe_secret_key
+
+    print(stripe_secret_key)
+
+    intent = stripe.PaymentIntent.create(
+            amount=stripe_total,
+            currency=settings.STRIPE_CURRENCY,
+        )
 
     context = {
         'order_form': order_form,
+        'stripe_public_key': stripe_public_key,
+        'client_secret': intent.client_secret,
     }
 
     return render(request, template, context)
--- ciswag/settings.py
+++ ciswag/settings.py
@@ -13,6 +13,10 @@ import os
 
 from pathlib import Path
 
+if os.path.isfile("env.py"):
+    import env
+
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -165,3 +169,4 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 STRIPE_PUBLIC_KEY = os.environ.get('STRIPE_PUBLIC_KEY')
 STRIPE_SECRET_KEY = os.environ.get('STRIPE_SECRET_KEY')
+STRIPE_CURRENCY = 'usd'
