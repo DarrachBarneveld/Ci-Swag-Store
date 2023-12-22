4413d5e7e854f5b3852461545c124d346ad12992
DarrachBarneveld <130994206+DarrachBarneveld@users.noreply.github.com>
2023-12-22 20:17:10 +0000

    Added input validation for product card

--- cart/views.py
+++ cart/views.py
@@ -16,20 +16,18 @@ def view_cart(request):
 def add_to_cart(request, item_id):
     """ Add a quantity of the specified product to the shopping cart """
 
-    print(item_id)
+    quantity = int(request.POST.get('quantity'))
+
     if int(item_id) >= 26:
         product = get_object_or_404(Program, pk=item_id)
     else:
         product = get_object_or_404(Product, pk=item_id )
 
-
-
-    quantity = request.POST.get('quantity')
     redirect_url = request.POST.get('redirect_url')
     cart = request.session.get('cart', {})
 
     if item_id in list(cart.keys()) and quantity:
-        cart[item_id]['quantity'] += int(quantity)
+        cart[item_id] += int(quantity)
         messages.success(request, f'Updated {product.name} quantity to {cart[item_id]}')
     else:
         cart[item_id] = 1
@@ -43,15 +41,14 @@ def add_to_cart(request, item_id):
 def remove_from_cart(request, item_id):
     """Remove the item from the shopping cart"""
 
-    item_type = request.POST.get('item_type')
     cart = request.session.get('cart', {})
     redirect_url = request.POST.get('redirect_url')
 
 
-    if item_type == 'product':
-        product = get_object_or_404(Product, pk=item_id)
+    if int(item_id) >= 26:
+        product = get_object_or_404(Program, pk=item_id)
     else:
-        product = get_object_or_404(Program, pk=item_id )
+        product = get_object_or_404(Product, pk=item_id )
 
     try:
         cart.pop(item_id)
